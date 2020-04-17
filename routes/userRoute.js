de3be8e815a7a7529a565e64a1b21f9728a86684
Dofiii <38855294+Dofiii@users.noreply.github.com>
2020-04-17 15:36:02 +0300

    db jutut

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+
 router.get('/', userController.user_list_get);
 
 router.get('/:id', userController.user_get);
